d9023e18fac09cd6524b53a9dd783a09d997f063
calvinsuzuki <calvinssdcamargo@gmail.com>
2025-01-17 20:02:20 -0300

    Removed PositionHandle; Merge functions to Planar2DOFRobot

--- Planar2DOFRobot.m
+++ Planar2DOFRobot.m
@@ -1,9 +1,10 @@
-classdef Planar2DOFRobot
+classdef Planar2DOFRobot < handle
     properties
         L1 = 2;  % Length of the first link
         L2 = 1.5; % Length of the second link
         xBase = 0; % X-coordinate of the base of the robot
         yBase = 0; % Y-coordinate of the base of the robot
+        Position = [0, 0]; % Current position of the end-effector
     end
     
     methods
@@ -18,16 +19,14 @@ classdef Planar2DOFRobot
             end
         end
         
-        function plotRobot(obj, q1, q2, figHandle, holdFig)
-            % Joint angles (in radians)
-            theta1 = q1; theta2 = q2;
-
+        function obj = plotRobot(obj, q1, q2, figHandle, holdFig)
+            obj.Position = [q1, q2]; % Update the end-effector position
             % Calculate joint positions
-            x0 = obj.xBase; y0 = obj.yBase; % Base of the robot
-            x1 = x0 + obj.L1 * cos(theta1); % End of the first link
-            y1 = y0 + obj.L1 * sin(theta1);
-            x2 = x1 + obj.L2 * cos(theta1 + theta2); % End-effector position
-            y2 = y1 + obj.L2 * sin(theta1 + theta2);
+            x0 = obj.xBase; y0 = obj.yBase;
+            x1 = x0 + obj.L1 * cos(q1);
+            y1 = y0 + obj.L1 * sin(q1);
+            x2 = x1 + obj.L2 * cos(q1 + q2);
+            y2 = y1 + obj.L2 * sin(q1 + q2);
 
             if ~holdFig
                 figure(figHandle);
@@ -44,6 +43,9 @@ classdef Planar2DOFRobot
                 plot(xOuter, yOuter, 'y--', 'LineWidth', 1.5); % Outer circle
                 plot(xInner, yInner, 'y--', 'LineWidth', 1.5); % Inner circle
             end
+            
+            % Plot the background image
+            image('CData', imread('aerotech_bkg.jpg'), 'XData', [0 800], 'YData', [0 600]);
 
             % Plot the links
             plot([x0, x1], [y0, y1], 'b-', 'LineWidth', 2); % First link
@@ -59,11 +61,75 @@ classdef Planar2DOFRobot
             xC = r * cos(theta) + r; % X-coordinates of the "C" before rotation
             yC = r * sin(theta); % Y-coordinates of the "C" before rotation
 
-            rotationMatrix = rz(theta1 + theta2);  % Use rotation matrix from Featherstone library
+            rotationMatrix = rz(q1 + q2);  % Use rotation matrix from Featherstone library
             rotatedCoords = rotationMatrix(1:2,1:2)' * [xC; yC];
             xC = rotatedCoords(1, :) + x2;
             yC = rotatedCoords(2, :) + y2;
             plot(xC, yC, 'k-', 'LineWidth', 2);
+        end
+
+        function response = ramp(obj, newPosition, time_span, sampling_time, PID)
+            time = 0:sampling_time:time_span;
+            steps = length(time);
+            xRamp = linspace(obj.Position(1), newPosition(1), steps);
+            yRamp = linspace(obj.Position(2), newPosition(2), steps);
+            setpoint = [xRamp', yRamp'];
+            [state_history, ~] = planarPID(time, setpoint, [obj.Position(1) obj.Position(2) 0 0 0 0], PID);
+
+            response = state_history;
+        end
+        
+        function ramp = generateRamp(obj, newPosition, steps)
+            xRamp = linspace(obj.Position(1), newPosition(1), steps);
+            yRamp = linspace(obj.Position(2), newPosition(2), steps);
+            ramp = [xRamp', yRamp'];
+        end
+
+        function [q1, q2] = ikine(obj, x, y)
+            % Move x y to base frame
+            x = x - obj.xBase;
+            y = y - obj.yBase;
+
+            % Compute inverse kinematics for a 2-DOF planar robot
+            r = sqrt(x^2 + y^2);
+            outerRadius = obj.L1 + obj.L2;
+            innerRadius = abs(obj.L1 - obj.L2);
+        
+            if r > outerRadius
+                fprintf('Warning: Target point is outside the reachable workspace.');
+                x = x * outerRadius / r;
+                y = y * outerRadius / r;
+            elseif r < innerRadius
+                fprintf('Warning: Target point is outside the reachable workspace.');
+                x = x * innerRadius / r;
+                y = y * innerRadius / r;
+            end
+        
+            % Angle for the second joint
+            cosTheta2 = (x^2 + y^2 - obj.L1^2 - obj.L2^2) / (2 * obj.L1 * obj.L2);
+            if cosTheta2 > 1
+                cosTheta2 = 1;
+            elseif cosTheta2 < -1
+                cosTheta2 = -1;
+            end
+        
+            sinTheta2 = sqrt(1 - cosTheta2^2); % Assume positive elbow up solution
+            theta2 = atan2(sinTheta2, cosTheta2);
+        
+            % Angle for the first joint
+            k1 = obj.L1 + obj.L2 * cosTheta2;
+            k2 = obj.L2 * sinTheta2;
+            theta1 = atan2(y, x) - atan2(k2, k1);
+        
+            q1 = theta1;
+            q2 = theta2;
+        end
+
+        function getEndEffectorState(obj, robot_state)
+            % Get the end-effector state from the robot state
+            x0 = obj.xBase; y0 = obj.yBase; % Base of the robot
+            q1 = robot_state(1); q2 = robot_state(2);
+            x1 = x0
         end
     end
 end
--- interactive_robot.m
+++ interactive_robot.m
@@ -1,47 +1,31 @@
-% Interactive script
 clear; clc; close all;
 
-% Create robot instance
+% Set up interactive screen
 screenW = 800; screenH = 600;
 robot = Planar2DOFRobot(180, 120, screenW/2, screenH/2);
-
 figHandle = figure;
-robot.plotRobot(0, 0, figHandle, true);
-
-% Set up interactive screen
-image('CData', imread('aerotech_bkg.jpg'), 'XData', [0 screenW], 'YData', [0 screenH]);
 hold on;
 axis([0 screenW 0 screenH]);
 set(gca, 'YDir', 'reverse');
 title('Click to Move the Robot End-Effector');
 set(gca, 'XTick', [], 'YTick', []);
-lastPosition = PositionHandle([0, 0]);
-set(figHandle, 'WindowButtonDownFcn', @(~, ~) onMouseClick(robot, figHandle, lastPosition));
+robot.plotRobot(0, 0, figHandle, true);
+set(figHandle, 'WindowButtonDownFcn', @(~, ~) onMouseClick(robot, figHandle));
 
-function onMouseClick(robot, figHandle, lastPositionHandle)
+function onMouseClick(robot, figHandle)
     % Get clicked position
     point = get(gca, 'CurrentPoint');
-    x = point(1, 1) - 400;
-    y = point(1, 2) - 300;
-    last_p = lastPositionHandle.Position;
-    disp(lastPositionHandle.Position);
+    x = point(1, 1); y = point(1, 2);
+    [q1, q2] = robot.ikine(x, y);
     
-    [q1, q2] = inverseKinematics(x, y, robot.L1, robot.L2);
-    
-    time = 0:0.001:1;
-    % setpoint = ones(length(time),2) .* [q1 q2];
-    setpoint = lastPositionHandle.generateRamp([q1 q2], length(time));
-        [state_history, ~] = planarPID(time, setpoint, [last_p(1) last_p(2) 0 0 0 0], [200 30 40]);
-    % Save the state history for animation
-    save('state_history.mat', 'state_history');
-    save('setpoint.mat', 'setpoint');
-    for j = 1:50:length(time)
+    ramp_time = 1;
+    sampling_time = 0.001;
+    PID = [200 30 40];
+
+    response = robot.ramp([q1, q2], ramp_time, sampling_time, PID);
+    for j = 1:50:length(response)
         cla;
-        image('CData', imread('aerotech_bkg.jpg'), 'XData', [0 800], 'YData', [0 600]);
-        robot.plotRobot(state_history(j,1), state_history(j,2), figHandle, true);
+        robot.plotRobot(response(j,1), response(j,2), figHandle, true);
         pause(0.001);
     end
-    
-    % Update lastPosition
-    lastPositionHandle.Position = [state_history(end,1), state_history(end,2)];
 end
